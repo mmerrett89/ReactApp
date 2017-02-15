f0ac731e0f9d43afd1e4b36b5162c0d29d0d4d89
mmerrett89 <mmerrett89@hotmail.com>
2017-02-15 11:34:03 -0700

    comment

--- src/App.js
+++ src/App.js
@@ -2,20 +2,33 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>My React App</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+			My happy app
         </p>
-      </div>
-    );
-  }
+		<div className="Button">
+		<input type="button" id="myButton" value="Click Me!!!!!!!!"/>
+	  </div>
+		<div className="new">
+			<img src={logo}/>
+		
+      
+	   
+	  
+	  </div>	
+	  </div>
+    );	
+  } 
 }
+
+
 
 export default App;
